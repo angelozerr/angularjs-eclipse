e83c58630e4ab44909b7aa72c408895e0f93c54b
pascalleclercq <pascal.leclercq@gmail.com>
2014-10-03 23:24:20 +0200

    update license header

--- org.eclipse.angularjs.jsp.core/src/org/eclipse/angularjs/jsp/org/eclipse/jst/jsp/core/internal/validation/LocalizedMessage.java
+++ org.eclipse.angularjs.jsp.core/src/org/eclipse/angularjs/jsp/org/eclipse/jst/jsp/core/internal/validation/LocalizedMessage.java
@@ -1,13 +1,13 @@
-/*******************************************************************************
- * Copyright (c) 2007 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- * 
- * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+/**
+ *  Copyright (c) 2013-2014 Angelo ZERR.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v1.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Contributors:
+ *  Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ */
 package org.eclipse.angularjs.jsp.org.eclipse.jst.jsp.core.internal.validation;
 
 import java.util.Locale;
